4fa3ef515c3a03d668106c1d1037e5acd5c9c90d
Yingjian Ding <james.yingjian.ding@gmail.com>
2024-10-14 15:49:20 -0700

    adding more lip color

--- prototypes/clip_fashion_scope.py
+++ prototypes/clip_fashion_scope.py
@@ -7,7 +7,7 @@ model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 # Load an image where makeup is applied
-image = Image.open("/Users/yingjianding/Documents/imagesForFashionScope/2.jpg")
+image = Image.open("/Users/yingjianding/Documents/imagesForFashionScope/CHANELROUGE16022023.jpg")
 
 # Define makeup categories or descriptions to recognize
 makeup_descriptions = [
@@ -17,7 +17,10 @@ makeup_descriptions = [
     "highlighter", "bronzer", "winged eyeliner", "false eyelashes", "matte finish",
     "dewy skin", "glossy lips", "dark lipstick", "pastel eyeshadow", "no makeup look",
     "dramatic eye makeup", "rosy cheeks", "heavy foundation", "bright eyeshadow",
-    "gothic makeup", "colorful eyeliner", "subtle lip color", "shimmery eyeshadow"
+    "gothic makeup", "colorful eyeliner", "subtle lip color", "shimmery eyeshadow", 
+    # specific lipsticks
+    "Chanel Rouge Allure Velvet Luminous Matte Lip Color", "Chanel Rouge Coco Flash", "Chanel Rouge Allure Ink", 
+    "gucci rouge agrave l egrave vres voile sheer lipstick P452737", "gucci rouge a l eacute gant satin lipstick P452737"
 ]
 
 # Preprocess image and text for CLIP
